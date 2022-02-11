2e176654c952aac91cdcb2fdc8e8788d8d122772
756611752@qq.com <756611752@qq.com>
2022-02-11 11:32:30 +0800

    初始vue3+vite+ts+nutui

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import {
+  Button,
+  Cell,
+  Icon,
+  Tabbar,
+  Toast,
+  ShortPassword,
+  Price,
+  Layout,
+  Rate,
+  Popup,
+  Calendar,
+  Video,
+  NoticeBar,
+  NumberKeyboard,
+  CountDown,
+  Tag,
+  Badge,
+  SearchBar,
+  Avatar,
+  Menu,
+  MenuItem,
+  Popover,
+  Pagination,
+  Form,
+  FormItem,
+} from '@nutui/nutui';
+// import "@nutui/nutui/dist/styles/themes/default.scss";
+
+const nutuiComponents = [
+  Button,
+  Cell,
+  Form,
+  FormItem,
+  Icon,
+  Tabbar,
+  Toast,
+  ShortPassword,
+  Price,
+  Layout,
+  Rate,
+  Popup,
+  Calendar,
+  Video,
+  NoticeBar,
+  NumberKeyboard,
+  CountDown,
+  Tag,
+  Badge,
+  SearchBar,
+  Avatar,
+  Menu,
+  MenuItem,
+  Popover,
+  Pagination,
+];
+
+const app = createApp(App);
+
+nutuiComponents.forEach((item) => {
+  app.use(item);
+});
+
+app.mount('#app');
---
+++ src/test-components/NumberKeyboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <nut-numberkeyboard :visible="false">
+  </nut-numberkeyboard>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+export default defineComponent({
+  name: "NumberKeyboard",
+  setup() {},
+});
+</script>
